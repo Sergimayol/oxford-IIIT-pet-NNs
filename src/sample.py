264ed887c6c659223eefc87307a7ce771a7b3c26
Sergimayol <sergi2808@gmail.com>
2023-12-24 16:16:08 +0100

    Update sample.py

--- src/sample.py
+++ src/sample.py
@@ -7,6 +7,7 @@ from model import CatDogClassifier
 from utils import DATA_DIR, read_image
 
 if __name__ == "__main__":
+    """
     label_map = {0: "cat", 1: "dog"}
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     path = os.path.join(DATA_DIR, "models", "cdc-e020950a-ad2a-4859-94a2-08a39fbe4892-100.pth")
@@ -66,3 +67,39 @@ if __name__ == "__main__":
         print(predicted.shape)
         plt.imshow(predicted.cpu().numpy().squeeze())
         plt.show()
+    """
+    from ultralytics import YOLO
+
+    model = YOLO("./runs/detect/train5/weights/best.pt")
+    model.to("cuda")
+    img1 = read_image(os.path.join(DATA_DIR, "tests", "c.jpg"))
+    img2 = read_image(os.path.join(DATA_DIR, "tests", "d.jpg"))
+
+    results = model(img2)
+    result = results[0]
+    import cv2
+
+    image = cv2.imread(os.path.join(DATA_DIR, "tests", "d.jpg"))
+
+    for box in result.boxes:
+        class_id = result.names[box.cls[0].item()]
+        cords = box.xyxy[0].tolist()
+        cords = [round(x) for x in cords]
+        conf = round(box.conf[0].item(), 2)
+        print(f"[INFO] Object type: {class_id}, Coordinates: {cords}, Probability: {conf}")
+        cv2.rectangle(image, (cords[0], cords[1]), (cords[2], cords[3]), (0, 255, 0), 2)
+        cv2.putText(
+            image,
+            f"{class_id} {conf}",
+            (cords[0], cords[1] - 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.7,
+            (0, 255, 0),
+            2,
+        )
+
+
+    image = cv2.resize(image, (800, 600))
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
